a45e17046b07512770dc0c078409050ca6ca4dbd
Derek Schindelman <dschindelman@gmail.com>
2015-04-30 09:42:58 -0500

    Cleaned up code

--- www/js/directives/styleFor.js
+++ www/js/directives/styleFor.js
@@ -4,56 +4,30 @@ angular.module('hwo.ui').directive('hwoStyleFor', StyleForDirective);
 
 function StyleForDirective(colors, Assignment, Class) {
     var isDefined = angular.isDefined;
-    var isObject = angular.isObject;
 
     return {
         restrict: 'A',
-        require: '?ionItem',
-        link: function ($scope, $element, $attrs, itemCtrl) {
+        link: function ($scope, $element, $attrs) {
             $scope.$watch($attrs.hwoStyleFor, setStyle);
-            
-            var styleSetter = createStyleSetter($element, itemCtrl);
             
             function setStyle(value) {
                 if (isDefined(value)) {
-                    if (value instanceof Assignment) {
+                    if (isDefined(value.classId)) {
                         Class.get(value.classId).then(function (klass) {
-                            $element.css({
-                                'backgroundColor': klass.color,
-                                'color': colors.getTextColor(klass.color)
-                            });
+                            setColor(klass.color);
                         });
                     } else {
-                        $element.css({
-                            'backgroundColor': value,
-                            'color': colors.getTextColor(value)
-                         });
-                    } 
+                        setColor(value);
+                    }
                 }
+            }
+            
+            function setColor(color) {
+                $element.css({
+                    'backgroundColor': color,
+                    'color': colors.getTextColor(color)
+                });
             }
         }
     };
-    
-    function createStyleSetter($element, itemCtrl) {
-        if (!itemCtrl) return $element.css.bind($element);
-        
-        var children = $element.children(),
-            content;
-            
-        if (children.hasClass('item-content')) {
-            var length = children.length;
-            for (var i = 0; i < length; i++) {
-                var child = children.eq(i);
-                if (child.hasClass('item-content')) {
-                    content = child;
-                    break;
-                }
-            }
-        }
-        
-        return function (css) {
-            $element.css(css);
-            if (content) content.css(css);
-        };
-    }
 }
