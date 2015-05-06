8aef07410ef4430aad6beeffdee22aa59bc605d2
Derek Schindelman <dschindelman@gmail.com>
2015-05-06 08:13:25 -0500

    Work on master

--- www/js/controllers/assignmentListCtrl.js
+++ www/js/controllers/assignmentListCtrl.js
@@ -36,9 +36,71 @@ function AssignmentListCtrl($scope, Assignment, modifyAssignment, klass) {
     function loadAssignments() {
         Assignment.get(filter).then(function (assignments) {
             $scope.dateGroups = groupByDate(assignments);
+            
+            
+            var data = assignments.reduce(function (data, assignment) {
+                if (data.first) {
+                    var day = new Day(assignment.dueDateTime);
+                    var week = new Week(day);
+                    
+                    day.assignments.push(assignment);
+                    
+                    data.day = day;
+                    data.week = week;
+                    data.weeks.push(week);
+                    data.first = false;
+                } else {
+                    var day = data.day;
+                    if (isSameDay(day.date, assignment.dueDateTime)) {
+                        day.assignments.push(assignment);
+                    } else {
+                        var week = data.week;
+                        day = new Day(assignment.dueDateTime);
+                        day.assignments.push(assignment);
+                        
+                        data.day = day;
+                        
+                        if (week.contains(day.date)) {
+                            week.days.push(day);
+                        } else {
+                            week = new Week(day);
+                            data.week = week;
+                            data.weeks.push(week);
+                        }
+                    }
+                }
+                
+                return data;
+            }, { weeks: [], first: true });
+            
+            $scope.weeks = data.weeks;
         }, function (e) {
             alert(e.message);
         });
+    }
+    
+    function Week(day) {
+        var date = day.date;
+        this.start = new Date(date.getFullYear(), date.getMonth(), date.getDate() - date.getDay());
+        this.end = new Date(this.start.valueOf());
+        this.end.setDate(this.end.getDate() + 7);
+        this.days = [];
+        this.contains = function (date) {
+            return this.start.valueOf() <= date.valueOf()
+                && this.end.valueOf() <= date.valueOf();
+        };
+    }
+    
+    function Day(date) {
+        this.date = date;
+        this.assignments = [];
+    }
+    
+    function isSameDay(x, y) {
+        return !!x && !!y
+            && x.getDate() === y.getDate()
+            && x.getMonth() === y.getMonth()
+            && x.getFullYear() === y.getFullYear();
     }
     
     function groupByDate(assignments) {
@@ -61,11 +123,7 @@ function AssignmentListCtrl($scope, Assignment, modifyAssignment, klass) {
         
         return groups;
         
-        function isSameDay(x, y) {
-            return x.getDate() === y.getDate()
-                && x.getMonth() === y.getMonth()
-                && x.getFullYear() === y.getFullYear();
-        }
+        
         
         function createGroup(assignment) {
             return {
